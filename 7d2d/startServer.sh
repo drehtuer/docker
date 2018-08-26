ea9e1af4ad274e5bba0ad647fb895cffff9a9f30
drehtuer <drehtuer@drehtuer.de>
2018-08-26 18:21:13 +0200

    Fixed mistakes in shell script

--- 7d2d/startServer.sh
+++ 7d2d/startServer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 BIN="7DaysToDieServer.x86_64"
-LOG_FILE="log_$(date +"%Y%m%d_%H%M%S).txt"
+LOG_FILE="log_$(date +"%Y%m%d_%H%M%S").txt"
 
 # Make sure the file exists
 touch ${LOG_FILE}
 
 ./${BIN} configfile=${SDTD_CONFIG} -logfile ${LOG_FILE} -quit -batchmode -nographics -dedicated 1>>${LOG_FILE} 2>&1 &
 
-tail -f $LOG_FILE}
+tail -f ${LOG_FILE}
