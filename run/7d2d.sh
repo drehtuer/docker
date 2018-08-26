f8bcffe1a00cac1b012120e46e961cc3a6ab09e1
drehtuer <drehtuer@drehtuer.de>
2018-08-26 03:40:42 +0200

    Added server fixes, corrected entrypoint, made home dir writable

--- run/7d2d.sh
+++ run/7d2d.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 docker run \
     -p 26900:26900/tcp \
-    -p 26903:8082/tcp \
-    -p 26900:26900/udp \
+    -p 26901:8082/tcp \
     -p 26900:26900/udp \
     -p 26901:26901/udp \
     -p 26902:26902/udp \
     -v /var/data/docker/config/7d2d/serverconfig.xml:/opt/7d2d/server_data/serverconfig2.xml:ro \
     --restart no \
-    -d \
     drehtuer/7d2d:latest
