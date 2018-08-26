abd1cc36461fcb3ad924c1716402dd5bc1d46dea
root <root@rlyeh.drehtuer.net>
2018-08-26 20:34:08 +0200

    Fixed missing - in launch parameters

--- 7d2d/startServer.sh
+++ 7d2d/startServer.sh
@@ -5,6 +5,6 @@ LOG_FILE="log_$(date +"%Y%m%d_%H%M%S").txt"
 # Make sure the file exists
 touch ${LOG_FILE}
 
-./${BIN} configfile=${SDTD_CONFIG} -logfile ${LOG_FILE} -quit -batchmode -nographics -dedicated 1>>${LOG_FILE} 2>&1 &
+./${BIN} -configfile=${SDTD_CONFIG} -logfile ${LOG_FILE} -quit -batchmode -nographics -dedicated 1>>${LOG_FILE} 2>&1 &
 
 tail -f ${LOG_FILE}
--- run/7d2d.sh
+++ run/7d2d.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 docker run \
+    --network host \
     -p 8081:8081/tcp \
     -p 26900:26900/tcp \
     -p 26901:8082/tcp \
@@ -10,6 +11,6 @@ docker run \
     -v /var/data/docker/config/7d2d/serverconfig.xml:/opt/sdtd/serverconfig.xml:rw \
     -v /var/data/docker/config/7d2d/admin.xml:/opt/sdtd/admin.xml:rw \
     -v /var/data/docker/config/7d2d/webpermissions.xml:/opt/stdt/webpermissions.xml:rw \
-    -v /var/data/docker/persist/7d2d/:/opt/sdtd/_data:rw \
+    -v /var/data/docker/persist/7d2d/Save:/opt/sdtd/Save:rw \
     -d \
     drehtuer/7d2d:latest
