28da3610d8e478d866d35c94f9b27c805a354c1c
drehtuer <drehtuer@drehtuer.de>
2018-08-26 19:57:03 +0200

    Added telnet to container

--- run/7d2d.sh
+++ run/7d2d.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 docker run \
+    -p 8081:8081/tcp \
     -p 26900:26900/tcp \
     -p 26901:8082/tcp \
     -p 26900:26900/udp \
     -p 26901:26901/udp \
     -p 26902:26902/udp \
     --restart no \
+    -v /var/data/docker/config/7d2d/serverconfig.xml:/opt/sdtd/serverconfig.xml:rw \
+    -v /var/data/docker/config/7d2d/admin.xml:/opt/sdtd/admin.xml:rw \
+    -v /var/data/docker/config/7d2d/webpermissions.xml:/opt/stdt/webpermissions.xml:rw \
+    -v /var/data/docker/persist/7d2d/:/opt/sdtd/_data:rw \
     -d \
     drehtuer/7d2d:latest
