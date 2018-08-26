3b6b0bafdf9d212af7dda72bb5044386371a1fb2
drehtuer <drehtuer@drehtuer.de>
2018-08-26 12:27:27 +0200

    Added tail to 7d2d so it won't close after the server has been started

--- run/7d2d.sh
+++ run/7d2d.sh
@@ -7,4 +7,5 @@ docker run \
     -p 26902:26902/udp \
     -v /var/data/docker/config/7d2d/serverconfig.xml:/opt/7d2d/server_data/serverconfig2.xml:ro \
     --restart no \
+    -d \
     drehtuer/7d2d:latest
