170255bfbe0c896bf44964fbcd1c3835f95a2703
drehtuer <drehtuer@drehtuer.de>
2018-09-06 23:52:15 +0200

    Small fixes

--- dontstarvetogether_master/startDST.sh
+++ dontstarvetogether_master/startDST.sh
@@ -6,7 +6,7 @@ DST_LOG=${DST_STORAGE}/${DST_CLUSTER}/${DST_SHARD}/server_log.txt
 
 function download {
     echo "steamcmd update" >> ${DST_LOG}
-    ${STEAMCMD_PATH}/steamcmd.sh +login anonymous +force_install ${DST_DIR} +app_update 343050 validate +quit 1 1>>${DST_LOG} 2>&1
+    ${STEAMCMD_DIR}/steamcmd.sh +login anonymous +force_install ${DST_DIR} +app_update 343050 validate +quit 1 1>>${DST_LOG} 2>&1
 }
 
 function update {
@@ -27,6 +27,7 @@ function start {
 if [[ "$#" == "1" && "$1" == "update" ]]; then
     update
 else
+    touch ${DST_LOG}
     [ ! -f ${DST_BIN} ] && download
     [ "$?" == "0" ]     && start
     tail -f ${DST_LOG}
